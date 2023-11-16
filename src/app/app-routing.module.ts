feedb47909f242ac970619f3333820b7072eb9d2
Jasmit <Jasmit.Gharat@ext.saint-gobain.com>
2023-11-16 14:40:44 +0530

    login page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { VehicleDetailsComponent } from './pages/vehicle-details/vehicle-details
 import { ZoneCreationComponent } from './pages/zone-creation/zone-creation.component';
 import { ZoneMappingComponent } from './pages/zone-mapping/zone-mapping.component';
 import { ScannerCreationComponent } from './pages/scanner-creation/scanner-creation.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {path:'vehicleDetails',component:VehicleDetailsComponent},
   {path:'zoneCreation',component:ZoneCreationComponent},
   {path:'zoneMapping',component:ZoneMappingComponent},
   {path:'scannerCreation',component:ScannerCreationComponent},
+  {path:'login',component:LoginComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
